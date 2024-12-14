d06c406da4784564b364e2cc2370b9eb67ce4076
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-12-14 23:09:44 +0100

    Data visualization

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM python:3.11-alpine
 ENV HOME=/root
-RUN mkdir -p $HOME/.aws
-COPY .aws $HOME/.aws
 RUN mkdir -p /server
 WORKDIR /server
 COPY requirements.txt /server
 RUN pip3 install -r requirements.txt
 COPY server.py /server
 COPY config.json /server
+COPY airQ.db /server
+RUN mkdir -p /server/public
+COPY public/index.html /server/public
+COPY public/Lyne.js /server/public
 RUN chmod 744 /server/server.py
 EXPOSE 8080
 ENTRYPOINT ["python3", "/server/server.py"]
--- server.py
+++ server.py
@@ -1,74 +1,109 @@
-import json
-import logging
-import traceback
-
-import boto3 as aws
-
-from botocore.exceptions import ClientError
-from datetime import datetime, timezone
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import sqlite3
+class DB:
+    conn = None
+    cur = None
+    def addData(self, dataToAdd):
+        Temp=dataToAdd['Temp']
+        Hum=dataToAdd['Hum']
+        VOC=dataToAdd['VOC']
+        PM1=dataToAdd['PM1']
+        PM25=dataToAdd['PM2.5']
+        PM10=dataToAdd['PM10']
+
+        array = [Temp, Hum, VOC, PM1, PM25, PM10]
+
+        self.cur.execute("INSERT INTO airq VALUES (?, ?, ?, ?, ?, ?)",array )
+        self.conn.commit()
+    def getData(self):
+        
+        temp = self.cur.execute("SELECT temp FROM airq").fetchall()
+        hum = self.cur.execute("SELECT hum FROM airq").fetchall()
+        voc = self.cur.execute("SELECT voc FROM airq").fetchall()
+        pm1 = self.cur.execute("SELECT pm1 FROM airq").fetchall()
+        pm25 = self.cur.execute("SELECT pm25 FROM airq").fetchall()
+        pm10 = self.cur.execute("SELECT pm10 FROM airq").fetchall()
+        json = {
+            'Temp':temp,
+            'Hum':hum,
+            'VOC':voc,
+            'PM1':pm1,
+            'PM2.5':pm25,
+            'PM10':pm10
+        }
+
+        # for row in data:
+        #     json['Temp'].append(row[0])
+        #     json['Hum'].append(row[1])
+        #     json['VOC'].append(row[2])
+        #     json['PM1'].append(row[3])
+        #     json['PM2.5'].append(row[4])
+        #     json['PM10'].append(row[5])
+        return json
+    
+    def __init__(self):
+        if self.conn!=None:
+            return
+        self.conn = sqlite3.connect("airQ.db")
+        self.cur = self.conn.cursor()
+        self.cur.row_factory = lambda cursor, row: row[0]
+        #self.cur.execute('CREATE TABLE airq(temp, hum, voc, pm1, pm25, pm10)')
+        #self.cur.execute("IF (SELECT name FROM sysobjects where name='airq' ) NOT EXISTS BEGIN CREATE TABLE airq(temp, hum, voc, pm1, pm25, pm10)END")
 
 
 class S(BaseHTTPRequestHandler):
+    dataBase = DB()
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+    def do_GET(self):
+        if self.path=='/favico.ico':
+            self._set_response()
+
+        if self.path=='/':
+            self._set_response()
+            f = open('./public/index.html')
+            self.wfile.write(bytes(f.read(), "utf-8"))
+            f.close()
+
+        if self.path=='/Lyne.js':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/javascript')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            f= open('./public/Lyne.js')
+            self.wfile.write(bytes(f.read(), "utf-8"))
+
+        if self.path == "/getData":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            json_data = json.dumps(self.dataBase.getData())
+            self.wfile.write(bytes(json_data, "utf-8"))
+        
+        
+        
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        data = post_data.decode()
+        data = json.loads(post_data.decode())
+        
+        self.dataBase.addData(data)
+
         self._set_response()
-        
-        save_data(data)
-        
-        logging.debug(post_data)
-        logging.debug("POST request data"+ data)
 
+        #print("POST request data"+ data)
 
-def save_data(data):
-    try:
-        logging.info("Saving data to S3 Bucket.")
-            
-        logging.debug(datetime.now(timezone.utc))
-            
-        name = "data/" + str(datetime.now(timezone.utc))
-            
-        bucket.put_object(Body=data, Key=name)
-        bucket.wait_until_exists()
-            
-    except ClientError as error:
-        logging.error(traceback.format_exc()[:-1].replace("  ", "\t").replace("\n", "\n\t\t"))
     
 
 if __name__ == '__main__':
-    with open("config.json", "r") as config_file:
-        config = json.load(config_file)
-        
-    logging
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, S)
+    print("Starting http server...")
 
-    logging.basicConfig(
-        filename=config['logging_file'],
-        filemode="w",
-        level=logging.DEBUG,
-        force=True,
-        format='[%(asctime)s] %(levelname)s:\n\tModule: "%(module)s"\n\tFunction: "%(funcName)s"\n\tLine: %(lineno)d\n\t\t%(message)s\n',
-    )
-    
-    try:
-        bucket = aws.resource('s3').Bucket(config['bucket_name'])
-            
-        server_address = ('', config['port'])
-        
-        httpd = HTTPServer(server_address, S)
-        
-        logging.info("Starting...")
-        
-        httpd.serve_forever()
-        
-    except:
-        logging.error(traceback.format_exc()[:-1].replace("  ", "\t").replace("\n", "\n\t\t"))
-    finally:
-        logging.info("Stopping...")
-        logging.shutdown()
+    httpd.serve_forever()
