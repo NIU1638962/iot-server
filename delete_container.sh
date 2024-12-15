34aae76454194de74877c29d099764a6648cec41
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-12-16 00:26:43 +0100

    Revert "Fix bug"
    This reverts commit 41a436d897a2fe29623acd7b9541dac618ab2e3a.

--- delete_container.sh
+++ delete_container.sh
@@ -3,10 +3,10 @@ echo "Execute stop."
 bash stop_container.sh
 
 echo "Checking if server container exists and stopped."
-if [ "docker container ls -f status=exited -f name=server -a | tail +2 | head -1 | wc -m" != 0 ]
+if [ "docker container ls -f status=exited -f name=server -a | tail +2 | head -1 | wc -m" == 0 ]
 then
+    echo "Does not exist server container that is stopped."
+else
     echo "Does exist server container that is stopped. Removing server container."
     docker remove server
-else
-    echo "Does not exist server container that is stopped."
 fi
